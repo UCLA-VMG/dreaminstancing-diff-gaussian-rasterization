26557a410fddbb24e104cbc3a1fa5187fad15d49
gpi <gpimpale29@gmail.com>
2024-02-02 17:54:41 -0800

    reset

--- cuda_rasterizer/backward.h
+++ cuda_rasterizer/backward.h
@@ -29,17 +29,13 @@ namespace BACKWARD
 		const float2* means2D,
 		const float4* conic_opacity,
 		const float* colors,
-		const float* depths,
-		const float* alphas,
+		const float* final_Ts,
 		const uint32_t* n_contrib,
 		const float* dL_dpixels,
-		const float* dL_dpixel_depths,
-		const float* dL_dalphas,
 		float3* dL_dmean2D,
 		float4* dL_dconic2D,
 		float* dL_dopacity,
-		float* dL_dcolors,
-		float* dL_ddepths);
+		float* dL_dcolors);
 
 	void preprocess(
 		int P, int D, int M,
@@ -60,7 +56,6 @@ namespace BACKWARD
 		const float* dL_dconics,
 		glm::vec3* dL_dmeans,
 		float* dL_dcolor,
-		float* dL_ddepth,
 		float* dL_dcov3D,
 		float* dL_dsh,
 		glm::vec3* dL_dscale,
--- cuda_rasterizer/forward.cu
+++ cuda_rasterizer/forward.cu
@@ -266,13 +266,11 @@ renderCUDA(
 	int W, int H,
 	const float2* __restrict__ points_xy_image,
 	const float* __restrict__ features,
-	const float* __restrict__ depths,
 	const float4* __restrict__ conic_opacity,
-	float* __restrict__ out_alpha,
+	float* __restrict__ final_T,
 	uint32_t* __restrict__ n_contrib,
 	const float* __restrict__ bg_color,
-	float* __restrict__ out_color,
-	float* __restrict__ out_depth)
+	float* __restrict__ out_color)
 {
 	// Identify current tile and associated min/max pixel range.
 	auto block = cg::this_thread_block();
@@ -303,8 +301,6 @@ renderCUDA(
 	uint32_t contributor = 0;
 	uint32_t last_contributor = 0;
 	float C[CHANNELS] = { 0 };
-	float weight = 0;
-	float D = 0;
 
 	// Iterate over batches until all done or range is complete
 	for (int i = 0; i < rounds; i++, toDo -= BLOCK_SIZE)
@@ -357,8 +353,6 @@ renderCUDA(
 			// Eq. (3) from 3D Gaussian splatting paper.
 			for (int ch = 0; ch < CHANNELS; ch++)
 				C[ch] += features[collected_id[j] * CHANNELS + ch] * alpha * T;
-			weight += alpha * T;
-			D += depths[collected_id[j]] * alpha * T;
 
 			T = test_T;
 
@@ -372,11 +366,10 @@ renderCUDA(
 	// rendering data to the frame and auxiliary buffers.
 	if (inside)
 	{
+		final_T[pix_id] = T;
 		n_contrib[pix_id] = last_contributor;
 		for (int ch = 0; ch < CHANNELS; ch++)
 			out_color[ch * H * W + pix_id] = C[ch] + T * bg_color[ch];
-		out_alpha[pix_id] = weight; //1 - T;
-		out_depth[pix_id] = D;
 	}
 }
 
@@ -387,13 +380,11 @@ void FORWARD::render(
 	int W, int H,
 	const float2* means2D,
 	const float* colors,
-	const float* depths,
 	const float4* conic_opacity,
-	float* out_alpha,
+	float* final_T,
 	uint32_t* n_contrib,
 	const float* bg_color,
-	float* out_color,
-	float* out_depth)
+	float* out_color)
 {
 	renderCUDA<NUM_CHANNELS> << <grid, block >> > (
 		ranges,
@@ -401,13 +392,11 @@ void FORWARD::render(
 		W, H,
 		means2D,
 		colors,
-		depths,
 		conic_opacity,
-		out_alpha,
+		final_T,
 		n_contrib,
 		bg_color,
-		out_color,
-		out_depth);
+		out_color);
 }
 
 void FORWARD::preprocess(int P, int D, int M,
